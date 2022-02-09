ffbbcd212716a2ee0c6c0d44223978fd359c9e61
gerry <hkggerry@gmail.com>
2022-02-09 15:49:49 -0800

    commit tree 9665ca01b08e993613f83587820d6e70908cf70e

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function eyesOnMe(event){
+        console.log(`${focus()}:${blur()}`)
+    }
+    
+    return <button onClick={eyesOnMe}>Eyes on me</button>
+}
+
+
+function focus(){
+    console.log("Good!")
+}
+
+
+function blur(){
+    console.log("Hey! Eyes on me!")
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function keypad() {
+        console.log("Entering password...")
+    }
+    return (
+    <div>   
+        <input 
+            type="password"
+            onChange={keypad}
+        />     
+    </div>
+    )
+}
+
+export default Keypad;
